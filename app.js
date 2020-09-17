a836e5fd49c1189510f6bc3b54433ada2265eae2
asahapde <60831459+asahapde@users.noreply.github.com>
2020-09-16 22:47:14 -0400

    Fixed api calls and fully functional

--- app.js
+++ app.js
@@ -13,19 +13,55 @@ const notificationElement = document.querySelector(".notification");
 const KELVIN = 273;
 const key = "6d44ffa2eea4d53b94bfce797e93ebb2";
 
+const weather = {};
+
+weather.temperature = {
+    unit: "celcius"
+}
+
 if ('geolocation' in navigator){
     navigator.geolocation.getCurrentPosition(setPosition, showError);
 }
 else {
-    notificationElement.innerHTML = "<p>No location</p>";
+    notificationElement.innerHTML = '<p>No location</p>';
 }
 
 
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+function showError(error){
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
 
 
+function getWeather(latitude,longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
-function getWeather(latitude,longitue){
-    let api = 'api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitue}&appid=${key}';
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        })
+}
 
-    console.log(api);
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png" alt="">`;
+    tempElement.innerHTML = `<p>${weather.temperature.value}° C</p>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
